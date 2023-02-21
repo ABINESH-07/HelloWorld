b70205365bbf0a29a4e2bf0d2a15487daaee423b
ABINESH-07 <727721euit003@skcet.ac.in>
2023-02-21 12:48:55 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/pojoclass.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class pojoclass {
+	private int id;
+	private String name;
+	private String dept;
+	public int getId() {
+		return id;
+	}
+
+
+	this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+ 
+}
